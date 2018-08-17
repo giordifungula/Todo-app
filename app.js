97fc42e6cc80c802625da33b6f462f9711d1af78
Giordi Fungula <giordi.fungula@gmail.com>
2018-08-17 12:09:27 +0200

    it is able to add and store

---
+++ app.js
@@ -0,0 +1,15 @@
+// //Our 
+// Our todoApp needs
+// --To able to add things
+ var list = [];
+function addTodo(item){
+	// the item passed will be added somewhere , but where ?
+	list.push(item);
+	console.log(list); //check the list so far
+}
+addTodo('jodi');
+addTodo('james');
+addTodo('john');
+// -- to be able to change things
+// -- to be able to delete things
+// -- to display the things
