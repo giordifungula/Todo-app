89355abcab315fb81eeebf5eeffbe4578fa429a0
Giordi Fungula <giordi.fungula@gmail.com>
2018-08-17 12:38:06 +0200

    showing items after each action

--- app.js
+++ app.js
@@ -5,22 +5,28 @@
 function addTodo(item){
 	// the item passed will be added somewhere , but where ?
 	list.push(item);
-	console.log(list); //check the list so far
+	// console.log(list); //check the list so far
+
+	displayTodo();//display the Things so far
 }
 
 // -- to be able to change things
 function changeTodo(position, text){
 	//change any item in array
 	list[position] = text; 
+
+	displayTodo();//display the Things so far
 }
 // -- to be able to delete things
 function deleteTodo(item){
 	//delete elements in array
 	list.splice(item, 1);
+
+	displayTodo();//display the Things so far
 }
 // -- to display the things
 
 function displayTodo() {
 	// body...
-	console.log(list);
+	console.log("My Todos", list);
 }
